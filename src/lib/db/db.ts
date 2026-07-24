b36b3824582cfd144354416b40f0a7dd2e65ae18
Ronald Gonzales <ronaldoamadorgonzales@gmail.com>
2026-07-24 14:26:28 +0800

    feat: setup tsx, configure db:seed script, fix fallback password, and seed database

--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
 import * as schema from "./schema";
 
-const connectionString = process.env.DATABASE_URL || "postgres://postgres:postgres@localhost:5432/itadakimasu_db";
+const connectionString = process.env.DATABASE_URL || "postgres://postgres:postgres_secure_password_123@localhost:5432/itadakimasu_db";
 
 const pool = new pg.Pool({
   connectionString,
